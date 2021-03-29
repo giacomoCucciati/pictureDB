11de70848fcbb34a52ce9021067d5e55974d31fb
Giacomo Cucciati <giacomo.cucciati@gmail.com>
2021-03-29 09:33:51 +0200

    first image loading

--- frontend/src/views/Mainpage.vue
+++ frontend/src/views/Mainpage.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
     Pippo {{firstvar}}
+    <div>
+      <b-img :src="imageUrl" fluid alt="Responsive image"></b-img>
+    </div>
+    <div>
+      <b-form-file v-model="fileList" class="mt-3" multiple plain></b-form-file>
+      {{imageUrl}}
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 
 export default {
   name: 'MainPage',
   data () {
     return {
-      firstvar: 'Pluto'
+      firstvar: 'Pluto',
+      imageUrl: "",
+      fileList: []
+    }
+  },
+  watch: {
+    fileList: function (val) {
+      if (val.length > 0) {
+        console.log(val[0])
+        this.imageUrl = URL.createObjectURL(val[0])
+      }
     }
   },
   mounted () {
-    axios.get('/api/test').then(response => {
-      console.log('response: ', response.data.msg)
-    })
+    // axios.get('/api/test').then(response => {
+    //   console.log('response: ', response.data.msg)
+    // })
+    console.log('mounted')
   }
 }
 </script>
